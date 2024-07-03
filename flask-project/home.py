1be1aa5b30069a4defb117f0c9e2931ed5bb9fec
Natalia Peguero (Brooke) <pegueronatalia@gmail.com>
2024-07-03 18:45:25 +0000

    adding database for user restrigation

--- flask-project/home.py
+++ flask-project/home.py
@@ -2,24 +2,47 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 import git
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'the key you generated'
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
+
+
 @app.route("/")
 def hello_world():
     return render_template('home.html', subtitle='Home Page', text='This is the home page!')
-    
+
+
 @app.route("/about")
 def second_page():
     return render_template('about.html', subtitle='about', text='This is the second page!')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
